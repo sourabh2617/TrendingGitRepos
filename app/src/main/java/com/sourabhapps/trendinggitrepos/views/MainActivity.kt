340c71b54095d83e1062204139bcbb71887c4d7b
sourabh <sharmasourabh355@gmail.com>
2022-07-03 23:05:42 +0530

    added resources and splash activity

--- app/src/main/java/com/sourabhapps/trendinggitrepos/views/MainActivity.kt
+++ app/src/main/java/com/sourabhapps/trendinggitrepos/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sourabhapps.trendinggitrepos
+package com.sourabhapps.trendinggitrepos.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sourabhapps.trendinggitrepos.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/sourabhapps/trendinggitrepos/views/SplashActivity.kt
@@ -0,0 +1,38 @@
+/*
+ * Created by Onkar Nene on 17/8/19 2:38 PM
+ *
+ * Copyright (c) 2019 Onkar Nene. All rights reserved.
+ */
+
+package com.sourabhapps.trendinggitrepos.views
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.sourabhapps.trendinggitrepos.R.layout
+
+class SplashActivity : AppCompatActivity(), Runnable {
+	
+	private val handler: Handler = Handler()
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(layout.activity_splash)
+	}
+	
+	override fun onStart() {
+		super.onStart()
+		handler.postDelayed(this, 2000)
+	}
+	
+	override fun onStop() {
+		super.onStop()
+		handler.removeCallbacks(this)
+	}
+	
+	override fun run() {
+		startActivity(Intent(this, MainActivity::class.java))
+		finish()
+	}
+}
